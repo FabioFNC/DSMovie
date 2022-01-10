2843d7db204474b935dc121c4b4781115012b22a
Fabio FNC <fabiohenrique.7670br@gmail.com>
2022-01-10 16:33:30 -0300

    Project created

---
+++ backend/src/main/java/br/com/fabiofnc/dsmovie/DsmovieApplication.java
@@ -0,0 +1,13 @@
+package br.com.fabiofnc.dsmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/fabiofnc/dsmovie/DsmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fabiofnc.dsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
